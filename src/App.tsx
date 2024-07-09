ef712ef208083c8e7956a8e06cfe1844dffac3a1
RafaelHDSV <rafaebolt@gmail.com>
2024-07-09 11:47:40 -0300

    feat: adding inputs functionalities

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
+import { MapPin, Calendar, ArrowRight } from 'lucide-react';
+
 export function App() {
 	return (
 		<main className='App'>
 			<div className='main-container'>
-				<p>Convide seus amigos e planeje sua próxima viagem!</p>
+				<div className='logo'>
+					<img src='/logo.svg' alt='logo.svg' />
+					<p>
+						Convide seus amigos e planeje sua próxima viagem!
+					</p>
+				</div>
 
-				<div className='input-container'>
-					<input type='text' placeholder='Para onde você vai?' />
-					<input type='text' placeholder='Quando?' />
+				<div className='search-container'>
+					<div className='input-container'>
+						<div className='input'>
+							<label htmlFor='where'>
+								<MapPin className='input-icon'></MapPin>
+							</label>
+							<input
+								id='where'
+								type='text'
+								placeholder='Para onde você vai?'
+							/>
+						</div>
 
-					<button>Continuar</button>
+						<div className='input'>
+							<label htmlFor='when'>
+								<Calendar className='input-icon'></Calendar>
+							</label>
+							<input
+								id='when'
+								type='text'
+								placeholder='Quando?'
+							/>
+						</div>
+					</div>
+
+					<button>
+						Continuar <ArrowRight className='input-icon' />
+					</button>
 				</div>
 
 				<span>
