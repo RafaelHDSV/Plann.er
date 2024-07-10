b3624d1b1ad774b51d30b3e70374b93b54517dee
RafaelHDSV <rafaebolt@gmail.com>
2024-07-10 16:04:16 -0300

    feat: adding activities container

--- src/pages/TripDetails/index.tsx
+++ src/pages/TripDetails/index.tsx
@@ -1,4 +1,4 @@
-import { Calendar, MapPin, Settings2 } from 'lucide-react';
+import { Calendar, CircleCheck, MapPin, Plus, Settings2 } from 'lucide-react';
 
 export function TripDetails() {
 	return (
@@ -21,6 +21,65 @@ export function TripDetails() {
 					</button>
 				</div>
 			</header>
+
+			<main>
+				<div className='activities'>
+					<header>
+						<h2>Atividades</h2>
+						<button className='primary-button'>
+							<Plus className='input-icon' />
+							Cadastrar Atividade
+						</button>
+					</header>
+
+					<div className='days'>
+						<div className='day'>
+							<div className='name'>
+								<span>Dia 17</span>
+								<span>Sábado</span>
+							</div>
+
+							<p>
+								Nenhuma atividade cadastrada nessa data.
+							</p>
+						</div>
+
+						<div className='day'>
+							<div className='name'>
+								<span>Dia 18</span>
+								<span>Domingo</span>
+							</div>
+
+							<div className='activities-day'>
+								<div className='activitie-day'>
+									<CircleCheck className='input-icon' />
+									<span>Academia em grupo</span>
+									<span>08:00h</span>
+								</div>
+
+								<div className='activitie-day'>
+									<CircleCheck className='input-icon' />
+									<span>Academia em grupo</span>
+									<span>08:00h</span>
+								</div>
+							</div>
+						</div>
+
+						<div className='day'>
+							<div className='name'>
+								<span>Dia 19</span>
+								<span>Segunda-feira</span>
+							</div>
+
+							<p>
+								Nenhuma atividade cadastrada nessa data.
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<aside>b</aside>
+			</main>
 		</div>
 	);
 }
