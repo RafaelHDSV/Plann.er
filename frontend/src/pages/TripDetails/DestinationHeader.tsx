a66263092e583d298e00688aa118ffe612aa690b
RafaelHDSV <rafaebolt@gmail.com>
2024-07-11 08:34:18 -0300

    feat: adding guests integration on api

--- frontend/src/pages/TripDetails/DestinationHeader.tsx
+++ frontend/src/pages/TripDetails/DestinationHeader.tsx
@@ -1,19 +1,46 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { api } from '../../lib/axios';
+import { format } from 'date-fns';
 import { MapPin, Calendar, Settings2 } from 'lucide-react';
 
 import { SecundaryButton } from '../../components/SecundaryButton';
 
+interface Trip {
+	id: string;
+	destination: string;
+	starts_at: string;
+	ends_at: string;
+	is_confirmed: boolean;
+}
+
 export function DestinationHeader() {
+	const { tripId } = useParams();
+	const [trip, setTrip] = useState<Trip | undefined>();
+
+	useEffect(() => {
+		api.get(`/trips/${tripId}`).then((response) =>
+			setTrip(response.data.trip)
+		);
+	}, [tripId]);
+
+	const displayedDate = trip
+		? format(trip.starts_at, "dd' de 'LLL")
+				.concat(' até ')
+				.concat(format(trip.ends_at, "dd' de 'LLL"))
+		: null;
+
 	return (
 		<header>
 			<div className='destination'>
 				<MapPin className='input-icon' />
-				<span>Florianópolis, Brazil</span>
+				<span>{trip?.destination}</span>
 			</div>
 
 			<div className='date-and-change'>
 				<div className='date'>
 					<Calendar className='input-icon' />
-					<span>17 a 23 de agosto</span>
+					<span>{displayedDate}</span>
 				</div>
 
 				<SecundaryButton>
--- frontend/src/pages/TripDetails/Guests.tsx
+++ frontend/src/pages/TripDetails/Guests.tsx
@@ -1,30 +1,55 @@
-import { CircleDashed, UserCog } from 'lucide-react';
+import { CheckCircle2, CircleDashed, UserCog } from 'lucide-react';
+import { useEffect, useState } from 'react';
+import { api } from '../../lib/axios';
+import { useParams } from 'react-router-dom';
+
+interface Participant {
+	id: string;
+	name: string | null;
+	email: string;
+	is_confirmed: boolean;
+}
 
 export function Guests() {
+	const { tripId } = useParams();
+	const [participants, setParticipants] = useState<Participant[]>([]);
+
+	useEffect(() => {
+		api.get(`/trips/${tripId}/participants`).then((response) =>
+			setParticipants(response.data.participants)
+		);
+	}, [tripId]);
+
 	return (
 		<div className='guest-container'>
 			<h2>Convidados</h2>
 
 			<div className='guests'>
-				<div className='guest'>
-					<div className='text'>
-						<span>Jessica White</span>
+				{participants.map((participant, index) => {
+					return (
+						<div className='guest' key={participant.id}>
+							<div className='text'>
+								<span>
+									{participant.name ??
+										`Convidado ${index}`}
+								</span>
 
-						<span>jessica.white44@yahoo.com</span>
-					</div>
+								<span>{participant.email}</span>
+							</div>
 
-					<CircleDashed className='input-icon' />
-				</div>
-
-				<div className='guest'>
-					<div className='text'>
-						<span>Dr. Rita Pacocha</span>
-
-						<span>lacy.stiedemann@gmail.com</span>
-					</div>
-
-					<CircleDashed className='input-icon' />
-				</div>
+							{participant.is_confirmed ? (
+								<CheckCircle2
+									className='input-icon'
+									style={{
+										color: 'var(--light-green)',
+									}}
+								/>
+							) : (
+								<CircleDashed className='input-icon' />
+							)}
+						</div>
+					);
+				})}
 			</div>
 
 			<button className='secundary-button'>
